a594f8e7b592c5dd0ab92218fc04b613e3b3f6ed
zjq <zjq@kmu.com>
2020-07-04 15:47:08 +0800

    第三次修改了user.java

--- src/com/kmu/user.java
+++ src/com/kmu/user.java
@@ -5,5 +5,15 @@ public class user {
         System.out.println("第一次创建并且使用git");
         System.out.println("**************");
         System.out.println("进行了一次修改！！！！！");
+        System.out.println("进行了一次修改！！！！！");
+        System.out.println("进行了一次修改！！！！！");
+        System.out.println("进行了一次修改！！！！！");
+        System.out.println("进行了一次修改！！！！！");
+        System.out.println("进行了一次修改！！！！！");
+        System.out.println("进行了一次修改！！！！！");
+        System.out.println("进行了一次修改！！！！！");
+        System.out.println("进行了一次修改！！！！！");
+        System.out.println("进行了一次修改！！！！！");
+        System.out.println("进行了一次修改！！！！！");
     }
 }

c5b67c3accfef7aff140f04d50f0d0edcc05db93
zjq <zjq@kmu.com>
2020-07-04 15:21:40 +0800

    进行了依次修改，并进行提交！！！！！！！

--- src/com/kmu/user.java
+++ src/com/kmu/user.java
@@ -3,5 +3,7 @@ package com.kmu;
 public class user {
     public static void main(String[] args) {
         System.out.println("第一次创建并且使用git");
+        System.out.println("**************");
+        System.out.println("进行了一次修改！！！！！");
     }
 }

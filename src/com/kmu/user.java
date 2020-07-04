67bfd44530dd8de2d69e5cdfd02d816625ceede3
zjq <zjq@kmu.com>
2020-07-04 16:11:30 +0800

    hotfix分支中的代码

--- src/com/kmu/user.java
+++ src/com/kmu/user.java
@@ -2,18 +2,11 @@ package com.kmu;
 
 public class user {
     public static void main(String[] args) {
-        System.out.println("第一次创建并且使用git");
-        System.out.println("**************");
-        System.out.println("进行了一次修改！！！！！");
-        System.out.println("进行了一次修改！！！！！");
-        System.out.println("进行了一次修改！！！！！");
-        System.out.println("进行了一次修改！！！！！");
-        System.out.println("进行了一次修改！！！！！");
-        System.out.println("进行了一次修改！！！！！");
-        System.out.println("进行了一次修改！！！！！");
-        System.out.println("进行了一次修改！！！！！");
-        System.out.println("进行了一次修改！！！！！");
-        System.out.println("进行了一次修改！！！！！");
-        System.out.println("进行了一次修改！！！！！");
+        System.out.println("我是hotfix分支中的代码");
+        System.out.println("我是hotfix分支中的代码");
+        System.out.println("我是hotfix分支中的代码");
+        System.out.println("我是hotfix分支中的代码");
+        System.out.println("我是hotfix分支中的代码");
+
     }
 }
